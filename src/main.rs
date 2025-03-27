20b56381cf8d6e28a1618630e4e517a19f8c87d5
talarys <d.simon.ssx0@gmail.com>
2025-03-27 17:35:48 +0000

    Working on it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
+use color_eyre::eyre::Ok;
+use pretty_hex::PrettyHex;
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
-    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {body}")
+    let image_bytes = get_cat_image_bytes().await.unwrap();
+    // only dump the first 200 bytes so our terminal survives the
+    // onslaught. this will panic if the image has fewer than 200 bytes.
+    println!("{:?}", &image_bytes[..200].hex_dump());
+}
+
+async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
+
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let client = reqwest::Client::default();
+
+    let image = client
+        .get(api_url)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Vec<CatImage>>()
+        .await?
+        .pop()
+        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+
+    Ok(client
+        .get(image.url)
+        .send()
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?
+        .to_vec())
 }
