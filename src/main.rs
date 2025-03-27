dc3386893e0f05dad8a9abde3ce72d9a684541c6
talarys <d.simon.ssx0@gmail.com>
2025-03-27 18:13:23 +0000

    Almost done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
-use color_eyre::eyre::Ok;
-use pretty_hex::PrettyHex;
+use reqwest::StatusCode;
 use serde::Deserialize;
+
+use axum::{
+    Router,
+    body::Body,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+};
 
 #[tokio::main]
 async fn main() {
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    // only dump the first 200 bytes so our terminal survives the
-    // onslaught. this will panic if the image has fewer than 200 bytes.
-    println!("{:?}", &image_bytes[..200].hex_dump());
+    let app = Router::new().route("/", get(root_get));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
+async fn root_get() -> Response<Body> {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+        Err(e) => {
+            println!("Something went wrong: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+        }
+    }
+}
+
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
     #[derive(Deserialize)]
     struct CatImage {
         url: String,
@@ -29,12 +52,21 @@ async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
 
-    Ok(client
+    let image_bytes = client
         .get(image.url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
-        .await?
-        .to_vec())
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(
+        image,
+        &artem::config::ConfigBuilder::new()
+            .target(artem::config::TargetType::HtmlFile)
+            .build(),
+    );
+
+    Ok(ascii_art)
 }
